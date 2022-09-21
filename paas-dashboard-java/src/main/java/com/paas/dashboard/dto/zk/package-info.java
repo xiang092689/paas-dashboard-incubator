70256519e70386297a1b3c5308fdbba49ec38aec
Lico-Tom <95178212+Lico-Tom@users.noreply.github.com>
2022-09-21 23:19:00 +0800

    the zk storage interface definition (#24)

--- paas-dashboard-java/src/main/java/com/paas/dashboard/dto/zk/package-info.java
+++ paas-dashboard-java/src/main/java/com/paas/dashboard/dto/zk/package-info.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -18,20 +18,3 @@
  */
 
 package com.paas.dashboard.dto.zk;
-
-import com.paas.dashboard.util.config.BaseConfig;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Setter
-@Getter
-@NoArgsConstructor
-public class ZooKeeperInstanceDto extends BaseConfig {
-
-    private String id;
-    private String name;
-    private String host;
-    private int port;
-
-}
